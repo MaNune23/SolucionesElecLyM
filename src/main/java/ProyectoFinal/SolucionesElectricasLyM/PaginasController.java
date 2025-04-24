778b1bcffb26f923585f26af01d4f6ff5f7cacee
Sergioml00 <smata20256@ufide.ac.cr>
2025-04-24 01:08:54 -0600

    Cambios Sergio

---
+++ src/main/java/ProyectoFinal/SolucionesElectricasLyM/PaginasController.java
@@ -0,0 +1,39 @@
+package ProyectoFinal.SolucionesElectricasLyM;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author sergi
+ */
+@Controller
+public class PaginasController {
+
+    @GetMapping("/Solicitar_Servicio")
+    public String mostrarFormularioServicio() {
+        return "Solicitar_Servicio"; // Este nombre debe coincidir con el HTML en /templates
+    }
+
+    @GetMapping("/resenas")
+    public String mostrarResenas() {
+        return "resenas"; // sin la extensión .html
+
+    }
+
+    @GetMapping("/contactanos")
+    public String mostrarChat() {
+        return "chat"; // archivo: chat.html en templates
+    }
+
+    @GetMapping("/productos")
+    public String mostrarProductos() {
+        return "productos"; // archivo: productos.html en templates
+    }
+
+    @GetMapping("/nuestrosproyectos")
+    public String mostrarProyectos() {
+        return "nuestrosproyectos"; // archivo: nuestrosproyectos.html en templates
+    }
+
+}
--- src/main/java/com/electricista/controller/ServiceController.java
+++ src/main/java/com/electricista/controller/ServiceController.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.electricista.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
 
 /**
  *
@@ -21,4 +16,3 @@ public class ServiceController {
         return "service_request";
     }
 }
-
